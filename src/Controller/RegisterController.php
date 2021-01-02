1e70eda892710be9ce198e5dd83c90f6589fdf6f
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-01-02 02:19:25 +0100

    generating the Register service && controller // Creating the first method to take the method to create a user in the controller from the service

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\Register;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/register", name="register_"
+ * Class RegisterController
+ * @package App\Controller
+ */
+class RegisterController extends AbstractController
+{
+
+    private $registerService;
+
+    public function __construct(Register $register)
+    {
+        $this->registerService = $register;
+    }
+
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(): Response
+    {
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
+        ]);
+    }
+}
