a5ee9c91d980dd5791dc897e176e802d8d8333ce
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-01-02 02:30:49 +0100

    RegisterType is done

--- src/Form/UserRegisterType.php
+++ src/Form/UserRegisterType.php
@@ -4,22 +4,25 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserRegisterType extends AbstractType
+class UserRegisterType extends ComponentType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('pseudo')
-            ->add('email')
-            ->add('address')
-            ->add('city')
-            ->add('postalCode')
-            ->add('password')
+            ->add('name', TextType::class, $this->getConfig('Nom*', 'Votre nom'))
+            ->add('surname', TextType::class, $this->getConfig('Prénom*', 'Votre prénom'))
+            ->add('pseudo', TextType::class, $this->getConfig('Pseudo*', 'Votre pseudo'))
+            ->add('email', EmailType::class, $this->getConfig('Email*', 'Votre email'))
+            ->add('address', TextType::class, $this->getConfig('Adresse', 'Si vous souhaitez nous indiquer votre adresse'))
+            ->add('city', TextType::class, $this->getConfig('Ville*', 'Votre ville de résidence'))
+            ->add('postalCode', TextType::class, $this->getConfig('Code postal', 'Si vous souhaitez nous indiquer votre code postal'))
+            ->add('password', PasswordType::class, $this->getConfig('Mot de passe', 'Votre mot de passe'))
         ;
     }
 
