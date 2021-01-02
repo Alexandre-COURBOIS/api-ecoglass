dc7bdf409c20c95d200d05cbce2c499ef3a688de
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-01-02 02:23:31 +0100

    Starting to write the method for user registration

--- src/Service/Register.php
+++ src/Service/Register.php
@@ -1,10 +1,31 @@
 <?php
 
-
 namespace App\Service;
 
+use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class Register
 {
+
+    /**
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     * @param EntityManagerInterface $em
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     *
+     * Methode permettant l'inscription de nouveaux utilisateurs
+     */
+    public function userRegister(Request $request, ValidatorInterface $validator, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new Users();
+
+        $data = json_decode($request->getContent(), true);
+
+
+    }
 
 }
