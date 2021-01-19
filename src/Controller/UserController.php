6dc2b66effd49b2df882400128b3381b0d960edd
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-01-19 16:37:52 +0100

    Create a method in controller && repository to get a user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UsersRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Json;
+
+
+/**
+ * @Route("/api/user", name="user_")
+ * Class RegisterController
+ * @package App\Controller
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/get/user", name="get_user", methods={"POST"})
+     * @param UsersRepository $usersRepository
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getUserInformations(UsersRepository $usersRepository, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $email = $data['email'];
+
+        $user = $usersRepository->getUserByEmail($email);
+
+        return new JsonResponse($user, 200);
+    }
+}
