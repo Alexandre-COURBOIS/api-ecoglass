c9887348db10515e69603b98a3078050dec9f3b8
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-01-21 13:19:51 +0100

    Creating the form to update User Contact informations

--- src/Form/UserContactInformationsType.php
+++ src/Form/UserContactInformationsType.php
@@ -3,31 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Users;
-use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserContactInformationsType extends AbstractType
+class UserContactInformationsType extends ComponentType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('pseudo')
-            ->add('email')
-            ->add('address')
-            ->add('city')
-            ->add('postalCode')
-            ->add('token')
-            ->add('resetToken')
-            ->add('password')
-            ->add('longitude')
-            ->add('latitude')
-            ->add('idFacebook')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('roles')
+            ->add('email', EmailType::class, $this->getConfig('Email*', 'Votre email'))
+            ->add('address', TextType::class, $this->getConfig('Adresse', 'Si vous souhaitez nous indiquer votre adresse'))
+            ->add('city', TextType::class, $this->getConfig('Ville*', 'Votre ville de résidence'))
+            ->add('postalCode', TextType::class, $this->getConfig('Code postal', 'Si vous souhaitez nous indiquer votre code postal'))
         ;
     }
 
