55c233362b45ec6f7306999de8e9ff924f46c387
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-03-03 15:16:10 +0100

    Creating method to send contact message and form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+/**
+ * Class ContactController
+ * @package App\Controller
+ * @Route ("/contact", name="contact_")
+ */
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/send/message", name="send_message")
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
      */
-    public function index(): Response
+    public function sendContactMessage(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+        $datas = json_decode($request->getContent(), true);
+
+        if ($datas !== null && $datas) {
+
+            $contact = new Contact();
+
+            $form = $this->createForm(ContactType::class, $contact);
+
+            $form->submit($datas);
+
+            $validate = $validator->validate($contact, null, 'Contact');
+
+            if (count($validate) !== 0) {
+                foreach ($validate as $error) {
+                    return new JsonResponse($error->getMessage(), Response::HTTP_BAD_REQUEST);
+                }
+            }
+
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return new JsonResponse("Nous avons bien receptionné votre message. Une réponse vous sera envoyée par email.", Response::HTTP_CREATED);
+
+        } else {
+            return new JsonResponse("Merci de renseigner correctement le formulaire", Response::HTTP_BAD_REQUEST);
+        }
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,21 +4,21 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContactType extends AbstractType
+class ContactType extends ComponentType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('message')
-            ->add('createdAt')
-            ->add('updatedAt')
-        ;
+            ->add('nom', TextType::class, $this->getConfig("Nom", 'Votre Nom'))
+            ->add('prenom', TextType::class, $this->getConfig("Prénom", 'Votre Prénom'))
+            ->add('email', EmailType::class, $this->getConfig("Votre email", "Votre email"))
+            ->add('message', TextareaType::class, $this->getConfig("Votre message", "Renseigner votre message"));
     }
 
     public function configureOptions(OptionsResolver $resolver)
