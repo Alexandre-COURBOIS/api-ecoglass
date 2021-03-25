f28b8f23e742413d50b69b8d4843a93e70966ee7
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-03-25 23:39:57 +0100

    Update command to get an email if cron is executed

--- src/Command/UpdateContainers.php
+++ src/Command/UpdateContainers.php
@@ -10,6 +10,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -23,13 +25,15 @@ class UpdateContainers extends Command
     private ContainerController $containerController;
     private EntityManagerInterface $manager;
     private ContainersRepository $containerRepo;
+    private MailerInterface $mailerInterface;
 
-    public function __construct(Container $container, ContainerController $containerController, EntityManagerInterface $manager, ContainersRepository $containersRepository)
+    public function __construct(Container $container, ContainerController $containerController, EntityManagerInterface $manager, ContainersRepository $containersRepository, MailerInterface $mailer)
     {
         $this->container = $container;
         $this->containerController = $containerController;
         $this->manager = $manager;
         $this->containerRepo = $containersRepository;
+        $this->mailerInterface = $mailer;
         
         parent::__construct();
     }
@@ -53,6 +57,14 @@ class UpdateContainers extends Command
                 '============',
                 '<fg=green>' .$response. '</>'
             ]);
+
+            $email = (new Email())
+                ->from('updateWithCron@ecoglass.fr')
+                ->to("courbois.alexandre76440@gmail.com")
+                ->subject("La tâche cron a bien été executée")
+                ->text("la tâche cron c'est bien executée celle-ci a renvoyé le message suivant : ".$response);
+
+            $this->mailerInterface->send($email);
 
             return Command::SUCCESS;
 
